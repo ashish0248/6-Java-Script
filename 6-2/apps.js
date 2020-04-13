b87ebe39574cb67f348c99132065f5b5fb4a4232
Your Name <hajime@hajimenoMacBook-Pro.local>
2020-04-13 17:48:31 +0900

    finish 6-2

---
+++ 6-2/apps.js
@@ -0,0 +1,12 @@
+var orange = 100;
+var apple = 80;
+
+if (orange < apple){
+    alert('りんごの方が' + (apple - orange)  + '円高い');
+}
+else if(orange > apple){
+    alert('みかんの方が ' + (orange - apple)  +　'円高い');
+}
+else{
+    alert('りんごとみかんは同じ' + (orange) + '円')
+}
---
+++ 6-2/appss.js
@@ -0,0 +1,9 @@
+var max = 100;
+var num = 1;
+var count = 0;
+
+while (num <= max){
+    num *= 2;
+    count++;
+}
+alert('１に２を'+ count + '回かけると'+ num + 'になり、１００を超えます');
